73eaf6feaf48c94a95e3faf1efb743d11efe5116
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-07-26 14:13:42 +0100

    creating tables

--- task_2.sql
+++ task_2.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create the authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    author_name VARCHAR(255) NOT NULL,
     biography TEXT,
     birthdate DATE
 );
