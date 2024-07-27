30a1386f254bd604cac9a60ea84467ae43b9348b
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-07-27 16:44:48 +0100

    insertion of multiple records

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO customer(customer_id INT PRIMARY KEY AUTO_INCREMENT, customer_name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,address VARCHAR(255) NOT NULL)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."),
+(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+(3, "Obed Ehoneah" , "eobed@sandtech.com" , "125 Happiness  Ave."),
+(4, "Nehemial Kamolu" , "nkamolu@sandtech.com" , "126 Happiness  Ave.")
